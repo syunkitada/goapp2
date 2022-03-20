827f9310578ec32461fedfa819c7f705cadca395
syunkitada <syun.kitada@gmail.com>
2022-03-20 23:13:57 +0900

    update test

--- pkg/lib/process_utils/process.go
+++ pkg/lib/process_utils/process.go
@@ -19,7 +19,8 @@ type Process struct {
 }
 
 func GetProcess(pid int) (process *Process, err error) {
-	path := "/proc/" + strconv.Itoa(pid)
+	pidStr := strconv.Itoa(pid)
+	path := "/proc/" + pidStr
 	_, tmpErr := os.Stat(path)
 	if tmpErr == nil {
 		// get cmdline
@@ -35,10 +36,26 @@ func GetProcess(pid int) (process *Process, err error) {
 		}
 
 		// get threads
+		var threads []Thread
+		var pidDirs []os.FileInfo
+		if pidDirs, err = ioutil.ReadDir(path + "/task"); err != nil {
+			return
+		}
+		for _, pidDir := range pidDirs {
+			if pidStr != pidDir.Name() {
+				var threadPid int
+				if threadPid, err = strconv.Atoi(pidDir.Name()); err != nil {
+					return
+				}
+				threads = append(threads, Thread{
+					Pid: threadPid,
+				})
+			}
+		}
 
 		// get children
 		var children []Process
-		childrenPath := path + "/task/" + strconv.Itoa(pid) + "/children"
+		childrenPath := path + "/task/" + pidStr + "/children"
 		if bytes, err = ioutil.ReadFile(childrenPath); err != nil {
 			return
 		}
@@ -58,6 +75,7 @@ func GetProcess(pid int) (process *Process, err error) {
 		process = &Process{
 			Pid:      pid,
 			Cmds:     cmds,
+			Threads:  threads,
 			Children: children,
 		}
 		return
--- pkg/lib/process_utils/process_test.go
+++ pkg/lib/process_utils/process_test.go
@@ -1,6 +1,7 @@
 package process_utils
 
 import (
+	"os"
 	"os/exec"
 	"testing"
 
@@ -10,26 +11,43 @@ import (
 func TestGetProcess(t *testing.T) {
 	a := assert.New(t)
 
-	pidCh := make(chan int)
-	command := exec.Command("sleep", "1")
-	go func(pidCh chan int) {
-		command.Start()
-		pidCh <- command.Process.Pid
-		command.Wait()
-	}(pidCh)
+	command1 := exec.Command("sleep", "1")
+	command1.Start()
+	pid1 := command1.Process.Pid
+
+	command2 := exec.Command("sleep", "1")
+	command2.Start()
+	pid2 := command2.Process.Pid
 
 	{
-		pid := <-pidCh
-		process, err := GetProcess(pid)
-		a.Equal(err, nil)
+		// 単体プロセスのテスト
+		process, err := GetProcess(pid1)
+		a.NoError(err)
 		expected := Process{
-			Pid:  process.Pid,
+			Pid:  pid1,
 			Cmds: []string{"sleep", "1"},
 		}
 		a.Equal(expected, *process)
+
+		// go testの子プロセスとしてsleepプロセスがあることを確認する
+		selfPid := os.Getpid()
+		selfProcess, err := GetProcess(selfPid)
+		a.NoError(err)
+		a.Equal(selfPid, selfProcess.Pid)
+		expectedChildren := []Process{
+			Process{Pid: pid1, Cmds: []string{"sleep", "1"}},
+			Process{Pid: pid2, Cmds: []string{"sleep", "1"}},
+		}
+		a.Equal(expectedChildren, selfProcess.Children)
+
+		// go testはマルチスレッドなので一定数以上のスレッドが見つかることを確認する
+		a.Greater(len(selfProcess.Threads), 2)
+		// threadのpidは親のPIDよりも後であることを確認する
+		a.Greater(selfProcess.Threads[0].Pid, selfPid)
 	}
 
 	{
+		// 存在しないPIDのテスト
 		process, err := GetProcess(-1)
 		a.Equal(err, nil)
 		var expectedProcess *Process
